a8341a5e23e6c19517c189f8a4d661495e562cb1
Léon Baudouin <leon@agence-belle-epoque.fr>
2020-10-08 11:49:19 +0200

    Added preload scene and gitignored rider

---
+++ Assets/Scripts/Global/Input/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Global.Input
+{
+	public class InputManager : MonoBehaviour
+	{
+		private void Awake()
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Global/Preload.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Global
+{
+	public class Preload : MonoBehaviour
+	{
+
+		// cant be 0 at start
+		private static int _goBackTo = -1;
+
+		private void Start()
+		{
+			Screen.fullScreenMode = FullScreenMode.Windowed;
+			if (_goBackTo == -1)
+			{
+				var sceneIndex = SceneManager.GetActiveScene().buildIndex;
+				SceneManager.LoadScene(sceneIndex + 1);
+			}
+
+			SceneManager.LoadScene(_goBackTo);
+		}
+
+#if UNITY_EDITOR
+
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		static void InitLoadingScene()
+		{
+			var sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+			if (sceneIndex != 0)
+			{
+				_goBackTo = sceneIndex;
+				SceneManager.LoadScene(0);
+			}
+		}
+
+#endif
+
+	}
+}
