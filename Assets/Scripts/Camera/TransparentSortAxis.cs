fd36c852f85aa68ec4ec912d08e1c2dadb55c120
Léon Baudouin <leon@agence-belle-epoque.fr>
2020-10-08 16:51:43 +0200

    Fixed Camera transparent sort axis

---
+++ Assets/Scripts/Camera/TransparentSortAxis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentSortAxis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+	    var cam = GetComponent<Camera>();
+	    cam.transparencySortMode = TransparencySortMode.CustomAxis;
+	    cam.transparencySortAxis = new Vector3(0, 1, 0);
+    }
+}
