0fb631d6c085d13b7f2f265c652de68854be6141
Juliette <juliette.sageaubriot@gmail.com>
2020-10-09 11:28:23 +0200

    feat : rename branch & create my new scene

---
+++ Assets/Scripts/Sound/SoundController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource audio;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audio.volume -= Time.deltaTime * 2;
+        audio.spatialBlend -= Time.deltaTime * 2;
+        Debug.Log(audio.volume);
+    }
+}
