d53776b94642bce572ad0cd120dcb73605d4260e
Léon Baudouin <leon@agence-belle-epoque.fr>
2020-10-11 17:54:59 +0200

    Feat : Added player senses switch with camera

--- Assets/Scripts/Player/PlayerSenses.cs
+++ Assets/Scripts/Player/PlayerSenses.cs
@@ -6,15 +6,53 @@ using UnityEngine.InputSystem;
 
 namespace Player
 {
-	enum SensesState
+	public enum SensesState
 	{
 		AllSenses,
 		Blind,
 		Deaf
 	}
+
+	[Serializable]
+	public class SensesSwitchEvent : UnityEvent<int, SensesState> {}
+
 	public class PlayerSenses : MonoBehaviour
 	{
-		private SensesState _state = SensesState.Deaf;
+		[SerializeField] private SensesState defaultState;
+		[SerializeField] private int maxSwitchCount = 3;
+		[SerializeField] private Camera mainCamera;
+		[SerializeField] private Camera blindCamera;
+		[SerializeField] private SensesSwitchEvent onSensesSwitch;
+		private int _switchCount = 0;
+
+		private SensesState _state;
+		private SensesState State {
+			get => _state;
+			set
+			{
+				switch (value)
+				{
+					case SensesState.Blind:
+						ToBlind(_state);
+						break;
+					case SensesState.Deaf:
+						ToDeaf(_state);
+						break;
+					case SensesState.AllSenses:
+						ToAllSenses(_state);
+						break;
+				}
+				_state = value;
+			}
+		}
+
+		private void Start()
+		{
+			if (onSensesSwitch == null)
+				onSensesSwitch = new SensesSwitchEvent();
+
+			State = defaultState;
+		}
 
 		private void OnEnable()
 		{
@@ -28,8 +66,9 @@ namespace Player
 
 		private void Switch(InputAction.CallbackContext context)
 		{
+			if(_switchCount >= maxSwitchCount) return;
 			SensesState newState = SensesState.AllSenses;
-			switch (_state)
+			switch (State)
 			{
 				case SensesState.Deaf:
 					newState = SensesState.Blind;
@@ -40,7 +79,29 @@ namespace Player
 				case SensesState.AllSenses:
 					return;
 			}
-			_state = newState;
+
+			_switchCount++;
+			State = newState;
+			onSensesSwitch.Invoke(_switchCount, State);
 		}
+
+		private void ToBlind(SensesState oldValue)
+		{
+			mainCamera.enabled = false;
+			blindCamera.enabled = true;
+		}
+
+		private void ToDeaf(SensesState oldValue)
+		{
+			mainCamera.enabled = true;
+			blindCamera.enabled = false;
+		}
+
+		private void ToAllSenses(SensesState oldValue)
+		{
+			mainCamera.enabled = true;
+			blindCamera.enabled = false;
+		}
+
 	}
 }
