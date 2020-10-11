a0f9aba4820459115672a8fbe7989e8d352c8468
Juliette <juliette.sageaubriot@gmail.com>
2020-10-11 15:22:15 +0200

    feat : update sound script

--- Assets/Scripts/Sound/SoundController.cs
+++ Assets/Scripts/Sound/SoundController.cs
@@ -5,18 +5,14 @@ using UnityEngine;
 public class SoundController : MonoBehaviour
 {
     // Start is called before the first frame update
-    AudioSource audio;
     void Start()
     {
-        audio = GetComponent<AudioSource>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        audio.volume -= Time.deltaTime * 2;
-        audio.spatialBlend -= Time.deltaTime * 2;
-        Debug.Log(audio.volume);
+        
     }
 }
