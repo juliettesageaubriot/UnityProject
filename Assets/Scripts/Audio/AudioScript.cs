4ee811d43ac054bec0394a65d87d163d5af4b160
Juliette <juliette.sageaubriot@gmail.com>
2020-10-28 16:08:32 +0100

    feat : add audio

---
+++ Assets/Scripts/Audio/AudioScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+
+    private AudioSource _source;
+    void Start()
+    {
+        _source = GetComponent<AudioSource>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T) && !_source.isPlaying)
+        {
+            _source.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/MuffleSoundScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MuffleSoundScript : MonoBehaviour
+{
+    public AudioMixerSnapshot inside;
+    public AudioMixerSnapshot outside;
+
+    void OnTriggerEnter(Collider other)
+    {
+        inside.TransitionTo(.4f);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        outside.TransitionTo(.8f);
+    }
+}
