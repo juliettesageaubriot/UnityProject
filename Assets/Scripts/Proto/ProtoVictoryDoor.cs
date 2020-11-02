ef427e6a1ba547d7fc5fd340a1f1f225256b271e
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-11-02 17:34:11 +0100

    First level proto

---
+++ Assets/Scripts/Proto/ProtoVictoryDoor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Proto
+{
+    public class ProtoVictoryDoor : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent onTriggerEnter;
+
+        private void Start()
+        {
+            if (onTriggerEnter == null)
+                onTriggerEnter = new UnityEvent();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            onTriggerEnter.Invoke();
+        }
+    }
+}
