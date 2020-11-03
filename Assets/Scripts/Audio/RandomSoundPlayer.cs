9b1ed36a3267c9bc7fb04be3ca6179e2ec1d0c63
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-11-03 15:15:09 +0100

    Add RandomSoundPlayer and SoundData scriptable object

---
+++ Assets/Scripts/Audio/RandomSoundPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Audio;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomSoundPlayer : MonoBehaviour
+{
+    [SerializeField] private SoundData sounds;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayRandomSound()
+    {
+        var randomIndex = Mathf.FloorToInt(Random.Range(0f, sounds.array.Length));
+        _audioSource.PlayOneShot(sounds.array[randomIndex]);
+    }
+}
