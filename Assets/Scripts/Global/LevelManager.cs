7c9dd27af3473b26e43a70a6278f777ca23aa1d6
Juliette <juliette.sageaubriot@gmail.com>
2020-11-04 16:57:47 +0100

    feat: add scene manager

---
+++ Assets/Scripts/Global/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Global
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private LevelsEnum nextLevel;
+
+        public void OnNextLevel()
+        {
+            var name = ScenesEnum.LevelsNames(nextLevel);
+            var test = SceneManager.GetSceneByName(name);
+            
+            Debug.Log(test);
+        }
+    }
+}
