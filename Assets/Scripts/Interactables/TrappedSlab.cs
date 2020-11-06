fe346fa4f8b036a771ed54e9fe22ecda1a2542c3
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-11-06 11:51:30 +0100

    Feat: Added break sound trapped slab and level2 to build

--- Assets/Scripts/Interactables/TrappedSlab.cs
+++ Assets/Scripts/Interactables/TrappedSlab.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using Player;
 using UnityEngine;
+using UnityEngine.Events;
 using Utils;
 
 namespace Interactables
@@ -11,12 +12,15 @@ namespace Interactables
         [SerializeField] private SingleUnityLayer playerLayer;
         [SerializeField] private Sprite brokenSprite;
         [SerializeField] private SingleUnityLayer brokenLayer;
+        [SerializeField] private UnityEvent onSlabBreak;
 
         private SpriteRenderer _renderer;
         private Collider2D _collider2D;
 
         private void Start()
         {
+            if (onSlabBreak == null)
+                onSlabBreak = new UnityEvent();
             _renderer = GetComponent<SpriteRenderer>();
             _collider2D = GetComponent<Collider2D>();
         }
@@ -24,6 +28,8 @@ namespace Interactables
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.layer != playerLayer.LayerIndex) return;
+            
+            onSlabBreak.Invoke();
             
             _renderer.sprite = brokenSprite;
             gameObject.layer = brokenLayer.LayerIndex;
