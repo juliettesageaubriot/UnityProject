4c94b85e988b85db59c15461db655fb771cae991
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-11-23 00:04:15 +0100

    Fixed fuel tuto

--- Assets/Scripts/UI/FuelTuto.cs
+++ Assets/Scripts/UI/FuelTuto.cs
@@ -4,39 +4,70 @@ using Player;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using Utils;
 
 namespace UI
 {
+    [RequireComponent(typeof(EventSequence))]
     public class FuelTuto : MonoBehaviour
     {
+        [SerializeField] private Button tutoPanel;
         [SerializeField] private PlayerSensesData playerSenses;
-        [SerializeField] private UnityEvent onTutoSwitch;
+        [SerializeField] private UnityEvent onStartTuto;
+        [SerializeField] private UnityEvent onFirstSwitch;
 
         private bool _isListeningInputs;
+        private EventSequence _endSequence;
 
         private void Start()
         {
-            if (onTutoSwitch == null)
-                onTutoSwitch = new UnityEvent();
+            _endSequence = GetComponent<EventSequence>();
+            playerSenses.SenseChangeEvent += HandleFirstSwitch;
+            if (onStartTuto == null)
+                onStartTuto = new UnityEvent();
+            if (onFirstSwitch == null)
+                onFirstSwitch = new UnityEvent();
         }
 
-        public void WaitForSwitch()
+        public void StartTuto()
         {
+            onStartTuto.Invoke();
             _isListeningInputs = true;
-            playerSenses.SenseChangeEvent += HandleSwitch;
+            tutoPanel.onClick.AddListener(HandlePassTuto);
+            if(InputManager.IsReady) InputManager.ActionMaps.Player.Switch.performed += HandlePassTuto;
         }
 
-        private void HandleSwitch(SensesState _)
+
+        // Overload for input events
+        private void HandlePassTuto(InputAction.CallbackContext obj)
+        { HandlePassTuto(); }
+        
+        private void HandlePassTuto()
         {
             _isListeningInputs = false;
-            playerSenses.SenseChangeEvent -= HandleSwitch;
-            onTutoSwitch.Invoke();
+            
+            _endSequence.StartSequence();
+            
+            tutoPanel.onClick.RemoveListener(HandlePassTuto);
+            if(InputManager.IsReady) InputManager.ActionMaps.Player.Switch.performed -= HandlePassTuto;
         }
 
+        private void HandleFirstSwitch(SensesState _)
+        {
+            playerSenses.SenseChangeEvent -= HandleFirstSwitch;
+            onFirstSwitch.Invoke();
+            _endSequence.StopSequence();
+        }
+        
         private void OnDisable()
         {
+            playerSenses.SenseChangeEvent -= HandleFirstSwitch;
             if (_isListeningInputs)
-                playerSenses.SenseChangeEvent -= HandleSwitch;
+            {
+                tutoPanel.onClick.RemoveListener(HandlePassTuto);
+                if(InputManager.IsReady) InputManager.ActionMaps.Player.Switch.performed -= HandlePassTuto;
+            }
         }
     }
 }
