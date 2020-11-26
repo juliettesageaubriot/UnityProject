2d516d73971b755fbbcb08d526e21cd6d3597cce
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-11-26 16:46:05 +0100

    Feat: Added blind canvas, cinematic and transition

--- Assets/Scripts/Camera/SensesCam.cs
+++ Assets/Scripts/Camera/SensesCam.cs
@@ -1,27 +1,56 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using Player;
+using UI;
 using UnityEngine;
 
-public class SensesCam : MonoBehaviour
+namespace Camera
 {
-    [SerializeField] private PlayerSensesData data;
-    [SerializeField] private Camera blindCamera;
-    [SerializeField] private Camera normalCamera;
-
-    private void OnEnable() { 
-        data.SenseInitEvent += UpdateCams;
-        data.SenseChangeEvent += UpdateCams;
-    }
-    private void OnDisable() { 
-        data.SenseInitEvent -= UpdateCams;
-        data.SenseChangeEvent -= UpdateCams;
-    }
-
-    private void UpdateCams(SensesState state)
+    public class SensesCam : MonoBehaviour
     {
-        blindCamera.enabled = state == SensesState.Blind;
-        normalCamera.enabled = state != SensesState.Blind;
+        [SerializeField] private PlayerSensesData data;
+        [SerializeField] private UnityEngine.Camera blindCamera;
+        [SerializeField] private UnityEngine.Camera normalCamera;
+        [SerializeField] private BlindCanvas blindCanvas;
+
+        private void OnEnable() { 
+            data.SenseInitEvent += UpdateCams;
+            data.SenseChangeEvent += UpdateCams;
+        }
+        private void OnDisable() { 
+            data.SenseInitEvent -= UpdateCams;
+            data.SenseChangeEvent -= UpdateCams;
+        }
+
+        private void UpdateCams(SensesState state)
+        {
+            switch (state)
+            {
+                case SensesState.Blind:
+                    ToBlind();
+                    break;
+                case SensesState.Deaf:
+                    ToSighted();
+                    break;
+                case SensesState.AllSenses:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
+        }
+
+        private void ToBlind()
+        {
+            blindCamera.enabled = true;
+            normalCamera.enabled = false;
+            blindCanvas.ToBlind();
+        }
+
+        private void ToSighted()
+        {
+            normalCamera.enabled = true;
+            blindCamera.enabled = false;
+            blindCanvas.ToSighted();
+        }
+    
     }
 }
---
+++ Assets/Scripts/UI/BlindCanvas.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace UI
+{
+    public class BlindCanvas : MonoBehaviour
+    {
+        [SerializeField] private UIFade mainBlindCanvas;
+        [SerializeField] private UIFade cinematicPanel;
+        [SerializeField] private VideoPlayer cinematic;
+        [SerializeField] private VideoClip toBlindClip;
+        [SerializeField] private VideoClip toSightedClip;
+
+        private float TransitionDuration => Mathf.Max((float)toBlindClip.length, mainBlindCanvas.FadeDuration);
+
+        public void ToBlind()
+        {
+            BlindTransition();
+        }
+        public void ToBlind(Action callback)
+        {
+            BlindTransition();
+            StartCoroutine(WaitForTransition(TransitionDuration, callback));
+        }
+        
+        public void ToSighted()
+        {
+            SightedTransition();
+        }
+        public void ToSighted(Action callback)
+        {
+            SightedTransition();
+            StartCoroutine(WaitForTransition(TransitionDuration, callback));
+        }
+
+        private void BlindTransition()
+        {
+            mainBlindCanvas.FadeIn();
+            cinematic.clip = toBlindClip;
+            
+            cinematic.frame = 0;
+            cinematic.Play();
+            cinematicPanel.FadeIn(0f);
+            StartCoroutine(WaitForTransition(TransitionDuration, () =>
+                { cinematicPanel.FadeOut(); }));
+        }
+
+        private void SightedTransition()
+        {
+            mainBlindCanvas.FadeOut();
+            cinematic.clip = toSightedClip;
+
+            cinematic.frame = 0;
+            cinematic.Play();
+            cinematicPanel.FadeIn(0f);
+            StartCoroutine(WaitForTransition(TransitionDuration, () =>
+                { cinematicPanel.FadeOut(); }));
+        }
+
+        private static IEnumerator WaitForTransition(float duration, Action callback)
+        {
+            yield return new WaitForSeconds(duration);
+            callback();
+        }
+    }
+}
