111a1623dbf83c89417726138567ab254a4b3613
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-11-29 14:17:49 +0100

    Feat: Changed fire sound and played it at random time

---
+++ Assets/Scripts/Audio/RandomTimeSound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class RandomTimeSound : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+        [SerializeField] private AudioClip audioClip;
+        [SerializeField] private bool startAtAwake;
+    
+        private void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _audioSource.clip = audioClip;
+            if (startAtAwake) _audioSource.Play();
+        }
+
+        private void Play()
+        {
+            var randomTime = Random.value * audioClip.length;
+            _audioSource.time = randomTime;
+            _audioSource.Play();
+        }
+    }
+}
