286a1b8686e8a0b68c0d4aeb7dbaaa92f7286e88
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-11-30 17:53:39 +0100

    Feat: Added sound on fuel

---
+++ Assets/Scripts/Audio/SoundFade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundFade : MonoBehaviour
+    {
+        [SerializeField] [Range(0, 1)] private float inVolume = 1f;
+        [SerializeField] [Range(0, 1)] private float outVolume = 0f;
+        [SerializeField] private float fadeDuration = 0f;
+        [SerializeField] private bool startIn = true;
+
+        private AudioSource _audioSource;
+        private void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            if (startIn)
+                FadeIn(0f);
+            else
+                FadeOut(0f);
+        }
+
+        public void FadeIn()
+        {
+            FadeIn(fadeDuration);
+        }
+        public void FadeIn(float duration)
+        {
+            _audioSource.DOFade(inVolume, duration);
+        }
+
+        public void FadeOut()
+        {
+            FadeOut(fadeDuration);
+        }
+        public void FadeOut(float duration)
+        {
+            _audioSource.DOFade(outVolume, duration);
+        }
+    }
+}
