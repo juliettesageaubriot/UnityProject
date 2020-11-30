7f9249f70eef54e2b40d324f9c44046dd54e922c
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-11-30 13:01:15 +0100

    Feat: Added puzzle

---
+++ Assets/OrderPuzzle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderPuzzle : MonoBehaviour
+{
+    [SerializeField] private OrderPuzzlePiece[] orderPuzzlePieces;
+
+    private void Start()
+    {
+        foreach (var orderPuzzlePiece in orderPuzzlePieces)
+            orderPuzzlePiece.SetPuzzle(this);
+    }
+
+    public void ActivatePuzzlePiece(OrderPuzzlePiece puzzlePiece)
+    {
+        var isCorrect = true;
+        
+        foreach (var piece in orderPuzzlePieces)
+        {
+            isCorrect &= piece.IsActivated;
+            if (puzzlePiece == piece) break;
+        }
+        
+        if (Array.IndexOf(orderPuzzlePieces, puzzlePiece) == orderPuzzlePieces.Length - 1 && isCorrect)
+            Completed();
+        if (!isCorrect) Failed();
+    }
+
+    private void Completed()
+    {
+        Debug.Log("Complete");
+    }
+
+    private void Failed()
+    {
+        Debug.Log("Failed");
+        foreach (var orderPuzzlePiece in orderPuzzlePieces)
+            orderPuzzlePiece.Desactivate();
+    }
+}
