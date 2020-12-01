c47491e17e63f407fcbadb8955e24b22646c901a
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-12-01 22:20:10 +0100

    Feat: Added slab animation

--- Assets/Scripts/Interactables/TrappedSlab.cs
+++ Assets/Scripts/Interactables/TrappedSlab.cs
@@ -9,28 +9,22 @@ namespace Interactables
 {
     public class TrappedSlab : PlayerKiller
     {
-        [SerializeField] private SingleUnityLayer playerLayer;
-        [SerializeField] private Sprite brokenSprite;
         [SerializeField] private UnityEvent onSlabBreak;
-
-        private SpriteRenderer _renderer;
 
         private void Start()
         {
             if (onSlabBreak == null)
                 onSlabBreak = new UnityEvent();
-            _renderer = GetComponent<SpriteRenderer>();
         }
 
         protected override void OnKill()
         {
-            onSlabBreak.Invoke();
             BreakSlab();
         }
 
         public void BreakSlab()
         {
-            _renderer.sprite = brokenSprite;
+            onSlabBreak.Invoke();
         }
         
         
