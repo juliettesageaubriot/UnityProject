5b4127b7b93277a6d167ce0b5fc4c100a7980e83
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-12-01 01:29:13 +0100

    Feat: Added torch animation

---
+++ Assets/Scripts/Graphics/TorchOrientation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Graphics
+{
+    public enum TorchOrientationEnum
+    {
+        Right,
+        Left,
+        Front
+    }
+    
+    public class TorchOrientation : MonoBehaviour
+    {
+        private static readonly int Orientation = Animator.StringToHash("Orientation");
+        
+        [SerializeField] private TorchOrientationEnum torchDirection;
+
+        private void Start()
+        {
+            var orientation = 0.5f;
+            switch (torchDirection)
+            {
+                case TorchOrientationEnum.Front:
+                    orientation = 0.5f;
+                    break;
+                case TorchOrientationEnum.Right:
+                    orientation = 1f;
+                    break;
+                case TorchOrientationEnum.Left:
+                    orientation = 0f;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            GetComponent<Animator>().SetFloat(Orientation, orientation);
+        }
+    }
+}
