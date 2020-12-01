d31029d46781d887c04cfb1f51a0f3062b40f860
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-12-01 21:31:44 +0100

    Feat: Added headphone check

--- Assets/Scripts/UI/SoundRotation.cs
+++ Assets/Scripts/UI/SoundRotation.cs
@@ -6,10 +6,22 @@ namespace UI
 {
     public class SoundRotation : MonoBehaviour
     {
-        [SerializeField] [Range(10.0f, 100.0f)] private float rotationSpeed = 50.0f;
+        [SerializeField] [Range(10.0f, 100.0f)] private float rotationSpeed = 50f;
+        [SerializeField] [Range(0f, 1f)] private float startRotation = 0f;
+        [SerializeField] private float distance = 200f;
+        [SerializeField] private GameObject emitter;
+        private float _rotation;
+
+        private void Start()
+        {
+            _rotation = startRotation * Mathf.PI * 2f;
+        }
+
         private void FixedUpdate()
         {
-            gameObject.transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+            _rotation += rotationSpeed * Time.deltaTime / 50f;
+            var newPosition = new Vector2(Mathf.Cos(_rotation) * distance, Mathf.Sin(_rotation) * distance);
+            emitter.transform.position = gameObject.transform.TransformPoint(newPosition);
         }
     }
 }
